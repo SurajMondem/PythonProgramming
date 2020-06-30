d4b560d8167d47002807b65e98820c297245fdfa
SurajMondem <surajmondem@gmail.com>
2020-06-30 14:16:43 -0400

    Day_1 Finished

--- Day_1/BasicOperation.py
+++ Day_1/BasicOperation.py
@@ -1,6 +1,19 @@
+# Day 1
+# Basic Operations
+
 print(2+3)
 print(5-1)
 print(6/2)
 print(3*7)
 print(7%7)
 print(2**3)
+
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Suraj'))     # String
+print(type("Suraj"))     # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'suraj'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
