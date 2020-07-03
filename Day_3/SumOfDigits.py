aff86b693cbc9e4ba29c8df96b8256483426e4b9
SurajMondem <surajmondem@gmail.com>
2020-07-03 16:12:03 -0400

    Code Cleanup

--- Day_3/SumOfDigits.py
+++ Day_3/SumOfDigits.py
@@ -1,9 +1,9 @@
-#Algorithm to Display the sum of individual digits in a number
+# Algorithm to Display the sum of individual digits in a number
 
 number = int(input("Enter the number: "))
 digitSum = 0
 
-while number !=0:
+while number != 0:
     temp = int(number) % 10
     digitSum = digitSum + temp
     number /= 10
